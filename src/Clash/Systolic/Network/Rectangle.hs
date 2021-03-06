547b213f1ef7f7df004e288760c0d289accd29dc
Alex McKenna <alexmckenna@outlook.com>
2021-03-06 16:15:16 +0100

    Add input / output delay to `Rect`
    The predefined rectangular network now optionally allows data
    entering or leaving the network to be delayed. Output delay is
    applied in a reverse order to input delay, meaning if both are
    delayed there is effectively no delay (as values still enter and
    leave at the same time), but the latency will increase accordingly.

--- src/Clash/Systolic/Network/Rectangle.hs
+++ src/Clash/Systolic/Network/Rectangle.hs
@@ -1,37 +1,24 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Clash.Systolic.Network.Rectangle
   ( Rect(..)
-  , InputDelay(..)
+  , Delay(..)
   ) where
 
 import Clash.Prelude
+import Data.Bifunctor
+import Data.Kind
 
 import Clash.Systolic.Cell
 import Clash.Systolic.Network
-import Data.Foldable as Foldable
-import Data.Kind
-
-
-{-# INLINE lastLE #-}
-lastLE :: forall n a. (1 <= n) => Vec n a -> a
-lastLE = leToPlus @1 @n last
-
-delayInput
-  :: forall n dom a
-   . ( HiddenClockResetEnable dom
-     , KnownNat n
-     , NFDataX a
-     )
-  => Index n
-  -> Signal dom a
-  -> Signal dom a
-delayInput n signal =
-  Foldable.foldr (const $ register undefined) signal [1..n]
 
 -- | For some use cases, it can be desirable to successively delay each input
--- along one or both dimensions of the network, in order to ensure that each
--- cell gets the corrent horizontal and vertical data at the same time.
+-- or output along one or both dimensions of the network. This helps to ensure
 --
-data InputDelay
+--   * that cells receive the correct input each cycle (when delaying input)
+--   * that the network output is dense (when delaying output)
+--
+data Delay
   = NoDelay
   | DelayHorizontal
   | DelayVertical
@@ -47,37 +34,93 @@ data Rect cell (n :: Nat) (m :: Nat) :: Type -> Type -> Type where
        , NFDataX a
        , NFDataX b
        )
-    => (Index n -> Index m -> cell)
-    -- ^ Function to generate configuration for cells from their index
-    -> InputDelay
-    -- ^ Input delay to the network
+    => { genCell  :: Index n -> Index m -> cell
+         -- ^ Function to generate configuration for cells from their index
+       , delayIn  :: Delay
+         -- ^ Delay to apply to network input
+       , delayOut :: Delay
+         -- ^ Delay to apply to network output
+       }
     -> Rect cell n m a b
+
+{-# INLINE lastRow #-}
+lastRow :: forall n m a. (1 <= n) => Vec n (Vec m a) -> Vec m a
+lastRow = leToPlus @1 @n last
+
+{-# INLINE lastColumn #-}
+lastColumn :: forall n m a. (1 <= m) => Vec n (Vec m a) -> Vec n a
+lastColumn = fmap (leToPlus @1 @m last)
+
+{-# INLINE bundleRect #-}
+bundleRect
+  :: forall dom n m a b
+   . (KnownNat n, KnownNat m)
+  => (Vec n (Signal dom a), Vec m (Signal dom b))
+  -> Signal dom (Vec n a, Vec m b)
+bundleRect = bundle . bimap bundle bundle
+
+{-# INLINE unbundleRect #-}
+unbundleRect
+  :: forall dom n m a b
+   . (KnownNat n, KnownNat m)
+  => Signal dom (Vec n a, Vec m b)
+  -> (Vec n (Signal dom a), Vec m (Signal dom b))
+unbundleRect = bimap unbundle unbundle . unbundle
+
+delayInputs
+  :: forall dom n m a b
+   . ( HiddenClockResetEnable dom
+     , KnownNat n
+     , KnownNat m
+     , NFDataX a
+     , NFDataX b
+     )
+  => Delay
+  -> (Vec n (Signal dom a), Vec m (Signal dom b))
+  -> (Vec n (Signal dom a), Vec m (Signal dom b))
+delayInputs = \case
+  NoDelay         -> id
+  DelayHorizontal -> first  (smap delayBy)
+  DelayVertical   -> second (smap delayBy)
+  DelayBoth       -> bimap  (smap delayBy) (smap delayBy)
+ where
+  delayBy i signal =
+    foldl (\s _ -> register undefined s) signal (replicate i ())
+
+delayOutputs
+  :: forall dom n m a b
+   . ( HiddenClockResetEnable dom
+     , KnownNat n
+     , KnownNat m
+     , NFDataX a
+     , NFDataX b
+     )
+  => Delay
+  -> (Vec n (Signal dom a), Vec m (Signal dom b))
+  -> (Vec n (Signal dom a), Vec m (Signal dom b))
+delayOutputs d =
+  -- A bit of a hack, but delaying each index by n-i / m-i is fiddly
+  bimap reverse reverse . delayInputs d . bimap reverse reverse
 
 instance SystolicNetwork (Rect cell n m a b) where
   type NetworkInput  (Rect _ n m a b) = (Vec n a, Vec m b)
   type NetworkOutput (Rect _ n m a b) = (Vec n a, Vec m b)
 
-  systolicNetwork (Rect gen inputDelay) inputs =
-    liftA2 (,) outA outB
+  systolicNetwork Rect{..} inputs =
+    bundleRect $ delayOutputs delayOut
+      (fmap fst <$> lastColumn cells, fmap snd <$> lastRow cells)
    where
-     outA   = bundle (fmap fst . lastLE <$> cells)
-     outB   = bundle (fmap snd <$> lastLE cells)
-     cells  = imap (map . build) (repeat @n (indicesI @m))
+    (inH, inV)  = delayInputs delayIn (unbundleRect inputs)
+    cells       = imap (map . build) (repeat @n (indicesI @m))
 
-     build n m =
-       let
-           inA | m == 0    = delayA n (fmap ((!!n) . fst) inputs)
-               | otherwise = fmap fst (cells !! n !! pred m)
+    build n m   =
+      let
+          inA | m == 0    = inH !! n
+              | otherwise = fmap fst (cells !! n !! pred m)
 
-           inB | n == 0    = delayB m (fmap ((!!m) . snd) inputs)
-               | otherwise = fmap snd (cells !! pred n !! m)
-        in
-           register undefined $ systolicCell (gen n m) (liftA2 (,) inA inB)
+          inB | n == 0    = inV !! m
+              | otherwise = fmap snd (cells !! pred n !! m)
 
-     (delayA, delayB) =
-       case inputDelay of
-         NoDelay          -> (const id,   const id)
-         DelayHorizontal  -> (delayInput, const id)
-         DelayVertical    -> (const id,   delayInput)
-         DelayBoth        -> (delayInput, delayInput)
+       in
+          register undefined $ systolicCell (genCell n m) (bundle (inA, inB))
 
